7be967ac88a9444d90839cb12f612200484fd77a
margOnline <margourey@gmail.com>
2025-06-03 18:15:11 +0100

    Use static data for the wordle solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react';
 import Wordle from './components/Wordle.jsx';
+import solutions from './constants/solutions.js'
 
 function App() {
   const [solution, setSolution] = useState(null)
-
+ 
   useEffect(() => {
-    fetch('http://localhost:3000/solutions')
-      .then(res => res.json())
-      .then(json => {
-        const randomSolution = json[Math.floor(Math.random() * json.length)]
-        setSolution(randomSolution.word)
-      })
+    const randomSolution = solutions[Math.floor(Math.random() * solutions.length)]
+    setSolution(randomSolution.word)
   }, [setSolution])
 
   return (

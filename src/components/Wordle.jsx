5a285d2092588d5832805cad85ad32553bca944f
margOnline <margourey@gmail.com>
2025-06-04 17:57:14 +0100

    Modal component to show result of game

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,19 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 import Grid from './Grid.jsx'
 import Keypad from './Keypad.jsx'
+import Modal from './Modal.jsx'
 import keys from '../constants/keys.js'
 
 export default function Wordle({ solution }) {
   const {currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = useWordle(solution)
+  const [showModal, setShowModal] = useState(false)
+  
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
 
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 1000);
+      window.removeEventListener('keyup', handleKeyup)
+    }
+
+    if (turn > 5){
+      setTimeout(() => setShowModal(true), 1000)
+      window.removeEventListener('keyup', handleKeyup)
+    }
+
     return () => window.removeEventListener('keyup', handleKeyup)
-  }, [handleKeyup])
+  }, [handleKeyup, isCorrect, turn])
 
   return (
     <div>
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
+      <div>Current guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
       <Keypad keys={keys} usedKeys={usedKeys} />
     </div>
